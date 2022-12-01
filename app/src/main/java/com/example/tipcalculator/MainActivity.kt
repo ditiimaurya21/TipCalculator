5cfd03fb45e9218c21bd24a251bc3143696bf53a
Diti Maurya <ditiimaurya@gmail.com>
2022-12-01 11:56:25 +0530

    Added all

---
+++ app/src/main/java/com/example/tipcalculator/MainActivity.kt
@@ -0,0 +1,57 @@
+package com.example.tipcalculator
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import com.example.tipcalculator.databinding.ActivityMainBinding
+import java.text.NumberFormat
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.calButton.setOnClickListener {calTip()}
+        binding.costOfServiceEditText.setOnKeyListener { view, keyCode, _ -> handleKeyEvent(view, keyCode)}
+
+        }
+    private fun calTip(){
+        val accText=binding.costOfServiceEditText.text.toString()
+        val cost=accText.toDoubleOrNull()
+        if(cost==null){
+            binding.tipResult.text=""
+            return
+        }
+        val tiPer=when(binding.tipOpts.checkedRadioButtonId){
+            R.id.opt1->0.20
+            R.id.opt2->0.18
+            else->0.15
+        }
+        var tip = tiPer*cost
+        val checkSwitch=binding.ruSwitch.isChecked
+        if(checkSwitch){
+            tip=kotlin.math.ceil(tip)
+        }
+        val formattedTip=NumberFormat.getCurrencyInstance().format(tip)
+        binding.tipResult.text= getString(R.string.tip_amount, formattedTip )
+
+    }
+
+    private fun handleKeyEvent(view: View, keyCode: Int): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+            // Hide the keyboard
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            return true
+        }
+        return false
+    }
+}
+
